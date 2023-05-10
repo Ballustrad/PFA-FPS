79cbf316b4db163719984d9a5ca32f8d35100afe
Ballustrad <ethan.azoulay@e-artsup.net>
2023-05-11 01:06:16 +0200

    GunUpdate

--- PFA-FPS/Assets/Scripts/BaseWeapon.cs
+++ PFA-FPS/Assets/Scripts/BaseWeapon.cs
@@ -6,16 +6,20 @@ public class BaseWeapon : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
+    private float nextTimeToFire = 0f;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -33,7 +37,13 @@ public class BaseWeapon : MonoBehaviour
                 target.TakeDamage(damage);
             }
 
-            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
     }
 }
