89bfae8881db385ab80610b1e472a88b56d4bc07
Ballustrad <ethan.azoulay@e-artsup.net>
2023-05-12 16:46:17 +0200

    pb

--- PFA-FPS/Assets/Scripts/Target.cs
+++ PFA-FPS/Assets/Scripts/Target.cs
@@ -1,14 +1,20 @@
 
 using System;
+using TMPro;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
     public float health = 50f;
-
+    public TextMeshProUGUI healthText;
+    private void Awake()
+    {
+        healthText.text = health.ToString();
+    }
     public void TakeDamage(float amountDamage)
     {
         health -= amountDamage;
+        healthText.text = health.ToString();
         if (health <= 0f )
         {
             Die();
