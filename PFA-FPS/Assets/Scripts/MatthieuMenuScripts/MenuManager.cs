d08435187a9c3c87eede2808162dd6e3e8d367eb
Matthieu FABRITIUS <matthieu.fabritius@laposte.net>
2023-05-22 10:39:05 +0200

    Adding menu sounds

--- PFA-FPS/Assets/Scripts/MatthieuMenuScripts/MenuManager.cs
+++ PFA-FPS/Assets/Scripts/MatthieuMenuScripts/MenuManager.cs
@@ -5,30 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip pointerEnterSound;
+
     public GameObject InstructionsPanel;
     public GameObject SettingsPanel;
     public GameObject CreditsPanel;
 
     public void PlayGame()
     {
+        audioSource.PlayOneShot(pointerEnterSound);
         Debug.Log("Play!");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void OpenInstructionsPanel()
     {
+        audioSource.PlayOneShot(pointerEnterSound);
         CloseAllPanels();
         InstructionsPanel.SetActive(true);
     }
 
     public void OpenSettingsPanel()
     {
+        audioSource.PlayOneShot(pointerEnterSound);
         CloseAllPanels();
         SettingsPanel.SetActive(true);
     }
 
     public void OpenCreditsPanel()
     {
+        audioSource.PlayOneShot(pointerEnterSound);
         CloseAllPanels();
         CreditsPanel.SetActive(true);
     }
@@ -42,6 +49,7 @@ public class MenuManager : MonoBehaviour
 
     public void QuitGame()
     {
+        audioSource.PlayOneShot(pointerEnterSound);
         Debug.Log("Quit");
         Application.Quit();
     }
